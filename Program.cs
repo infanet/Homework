f7680d07bc04d7f0ef13aacd42f10b867ef6e1ba
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-29 23:29:09 +0400

    Создал вывод массива что входит в него

--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 int symbolsArray = Convert.ToInt32(Console.ReadLine());
 string[] quantitySymbols = new string[symbolsArray];
 
-string[] inputSymbolsArray = InputSymbolsArray(quantitySymbols);
+InputSymbolsArray(quantitySymbols);
+
+InputArray(quantitySymbols);
 
 string[] InputSymbolsArray(string[] arr)
 {
@@ -13,4 +15,15 @@ string[] InputSymbolsArray(string[] arr)
         arr[i] = Console.ReadLine();
     }
     return arr;
+}
+
+void InputArray(string[] inputArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (i < inputArray.Length - 1) Console.Write($"\"{inputArray[i]}\", ");
+        else Console.Write($"\"{inputArray[i]}\"");
+    }
+    Console.WriteLine("]");
 }
