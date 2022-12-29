383434f4176070ce1901666533dd4e3344dd363f
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-29 23:27:23 +0400

    Создал метод для запросса символов для каждого индекса

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿Console.WriteLine("Введи количество символов в массиве: ");
 int symbolsArray = Convert.ToInt32(Console.ReadLine());
 string[] quantitySymbols = new string[symbolsArray];
+
+string[] inputSymbolsArray = InputSymbolsArray(quantitySymbols);
+
+string[] InputSymbolsArray(string[] arr)
+{
+    int index = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введи в {index + i}м индексе  массива символы: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
